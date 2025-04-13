6f1616ba2ae5379854231cd89c785f5e6cd72fc9
djirl13 <danieljordan13@gmail.com>
2025-04-13 20:24:22 +0100

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mosaic-match/',
   build: {
     outDir: 'dist',
     sourcemap: true
-  },
-  base: './'
+  }
 })
